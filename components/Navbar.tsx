d6097966f305ccecd96aef1e908d305cdf00c046
Jakob Campos <jakobcampos.jc@gmail.com>
2024-02-06 18:19:42 -0800

    Converted Navbar from flex to grid

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,48 +1,47 @@
-import Link from "next/link"
-import Image from "next/image"
-import { NAV_LINKS } from "@/constants"
-import Button from "./Button"
+import Link from "next/link";
+import Image from "next/image";
+import { NAV_LINKS } from "@/constants";
+import Button from "./Button";
 
 const Navbar = () => {
-  return (
-    <nav className="border-2 border-red-500 flexBetween max-container padding-container relative z-30 py-5">
-
-        <Image 
-            src="/menu.png"
-            alt="menu"
-            width={32}
-            height={32}
-            className="inline-block cursor-pointer lg:hidden"
-        />
-
-        <ul className="hidden h-full gap-8 lg:flex">
-            {NAV_LINKS.map((link) => (
-                <Link href={link.href} key={link.key} className="uppercase regular-14 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold">
-                    {link.label}
-                </Link>
-            ))}
-        </ul>
-
-        <Link href="/">
-            <Image src="/logo.png" alt="logo" width={100} height={100} />
-        </Link>
-
-        <div className="flex items-centers">
-            <Button 
-                type="button"
-                icon="/search.png"
-            />
-            <Button 
-                type="button"
-                icon="/user.png"
-            />
-             <Button 
-                type="button"
-                icon="/cart.png"
-            />           
-        </div>
-    </nav>
-  )
+    return (
+      <nav className="grid grid-cols-3 items-center justify-between border-2 border-red-500 px-20 py-5 relative z-30">
+  
+          {/* Left-aligned menu icon and navigation links */}
+          <div className="flex items-center justify-start">
+              <Image 
+                  src="/menu.png"
+                  alt="menu"
+                  width={32}
+                  height={32}
+                  className="inline-block cursor-pointer lg:hidden"
+              />
+              <ul className="hidden lg:flex items-center gap-8">
+                  {NAV_LINKS.map((link) => (
+                      <li key={link.key} className="text-sm uppercase text-gray-50 flexCenter whitespace-nowrap cursor-pointer transition-all hover:font-bold">
+                          <Link href={link.href} className="flex items-center justify-center">
+                            {link.label}
+                          </Link>
+                      </li>
+                  ))}
+              </ul>
+          </div>
+  
+          {/* Centered logo */}
+          <div className="flex justify-center">
+              <Link href="/">
+                  <Image src="/logo.png" alt="logo" width={75} height={75} />
+              </Link>
+          </div>
+          
+          {/* Right-aligned buttons */}
+          <div className="flex items-center justify-end">
+              <Button type="button" icon="/search.png" />
+              <Button type="button" icon="/user.png" />
+              <Button type="button" icon="/cart.png" />           
+          </div>
+      </nav>
+    );
 }
 
-export default Navbar
+export default Navbar;
