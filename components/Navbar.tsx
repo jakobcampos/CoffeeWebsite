53723d213e3af243a3ef34e7010f1380409089b5
Jakob Campos <jakobcampos.jc@gmail.com>
2024-02-08 12:26:18 -0800

    + Added hamburger menu functionality

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,17 @@
+// Navbar.tsx
+'use client';
+
 import Link from "next/link";
 import Image from "next/image";
-import { NAV_LINKS } from "@/constants";
 import Button from "./Button";
+import { NAV_LINKS } from "@/constants";
+import React, { useState } from 'react';
 
 const Navbar = () => {
+
+    // State to manage if the mobile menu is open
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
     return (
       <nav className="grid grid-cols-3 items-center justify-between border-2 border-red-500 px-20 py-5 relative z-30">
   
@@ -14,8 +22,25 @@ const Navbar = () => {
                   alt="menu"
                   width={32}
                   height={32}
+                  onClick={() => setIsMenuOpen(!isMenuOpen)} // Toggle menu visibility
                   className="inline-block cursor-pointer lg:hidden"
               />
+
+              {/* Mobile navigation menu */}
+              {isMenuOpen && (
+                  <ul className="absolute top-full left-0 w-full bg-white lg:hidden">
+                      {NAV_LINKS.map((link) => (
+                          <li key={link.key} className="text-xs font-sans font-normal tracking-widest uppercase text-gray-700 py-2 px-4 cursor-pointer">
+                              <Link href={link.href}>
+                                    <span className="inline-block pb-.5 border-b border-transparent hover:border-current transition duration-300 ease-in-out">
+                                        {link.label}
+                                    </span>
+                              </Link>
+                          </li>
+                      ))}
+                  </ul>
+              )}
+
               <ul className="hidden lg:flex items-center gap-6 sm:gap-7">
                   {NAV_LINKS.map((link) => (
                       <li key={link.key} className="text-xs font-sans font-normal tracking-widest uppercase text-gray-50 cursor-pointer transition-all">
