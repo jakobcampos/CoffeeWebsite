9faf6710cc6e4795c475622877a7a0675fd194b1
Jakob Campos <jakobcampos.jc@gmail.com>
2024-02-08 15:49:39 -0800

    + Added close/open icon functionality

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,8 @@
-'use client';
-import React, { useEffect, useState } from 'react';
+"use client";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
-import Button from './Button';
+import Button from "./Button";
 import { NAV_LINKS } from "@/constants";
 
 const Navbar = () => {
@@ -11,81 +11,85 @@ const Navbar = () => {
   useEffect(() => {
     const updateVhProperty = () => {
       const vh = window.innerHeight * 0.01;
-      document.documentElement.style.setProperty('--vh', `${vh}px`);
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
     };
 
-    window.addEventListener('resize', updateVhProperty);
+    window.addEventListener("resize", updateVhProperty);
     updateVhProperty(); // Call on mount to set initial value
 
-    return () => window.removeEventListener('resize', updateVhProperty);
+    return () => window.removeEventListener("resize", updateVhProperty);
   }, []);
 
   useEffect(() => {
-    if (isMenuOpen) {
-      document.body.style.overflow = 'hidden';
-    } else {
-      document.body.style.overflow = '';
-    }
+    document.body.style.overflow = isMenuOpen ? "hidden" : "";
   }, [isMenuOpen]);
 
-    return (
-      <nav className="grid grid-cols-3 items-center justify-between bg-primary lg:px-20 px-10 py-5 relative z-30">
-  
-          {/* Left-aligned menu icon and navigation links */}
-          <div className="flex items-center justify-start">
-              <Image 
-                  src="/menu.png"
-                  alt="menu"
-                  width={32}
-                  height={32}
-                  onClick={() => setIsMenuOpen(!isMenuOpen)} // Toggle menu visibility
-                  className="inline-block cursor-pointer lg:hidden"
-              />
+  return (
+    <nav className="grid grid-cols-3 items-center justify-between bg-primary lg:px-20 px-10 py-5 relative z-30">
+      {/* Left-aligned menu icon and navigation links */}
+      <div className="flex items-center justify-start">
+        <div
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+          className="inline-block cursor-pointer lg:hidden"
+        >
+          {/* Toggle menu visibility with dynamic src for the Image */}
+          <Image
+            src={isMenuOpen ? "/close.png" : "/menu.png"} // Assuming "/close.png" is your close icon
+            alt={isMenuOpen ? "Close menu" : "Open menu"}
+            width={32}
+            height={32}
+          />
+        </div>
 
-              {/* Mobile navigation menu */}
-              {isMenuOpen && (
-                  <ul className="mobileNav absolute top-full left-0 w-full bg-primary lg:hidden">
-                      {NAV_LINKS.map((link) => (
-                          <li key={link.key} className="text-sm font-sans font-normal tracking-widest uppercase text-gray-700 py-2 px-10 cursor-pointer">
-                              <Link href={link.href}>
-                                    <span className="inline-block pb-.5 border-b border-transparent hover:border-current transition duration-300 ease-in-out">
-                                        {link.label}
-                                    </span>
-                              </Link>
-                          </li>
-                      ))}
-                  </ul>
-              )}
+        {/* Mobile navigation menu */}
+        {isMenuOpen && (
+          <ul className="mobileNav absolute top-full left-0 w-full bg-primary lg:hidden">
+            {NAV_LINKS.map((link) => (
+              <li
+                key={link.key}
+                className="text-sm font-sans font-normal tracking-widest uppercase text-gray-700 py-2 px-10 cursor-pointer"
+              >
+                <Link href={link.href}>
+                  <span className="inline-block pb-.5 border-b border-transparent hover:border-current transition duration-300 ease-in-out">
+                    {link.label}
+                  </span>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        )}
 
-              <ul className="hidden lg:flex items-center gap-6 sm:gap-7">
-                  {NAV_LINKS.map((link) => (
-                      <li key={link.key} className="text-xs font-sans font-normal tracking-widest uppercase text-gray-50 cursor-pointer transition-all">
-                      <Link href={link.href}>
-                          <span className="whitespace-nowrap block pb-.5 border-b border-transparent hover:border-current transition duration-300 ease-in-out">
-                              {link.label}
-                          </span>
-                      </Link>
-                  </li>
-                  
-                  ))}
-              </ul>
-          </div>
-  
-          {/* Centered logo */}
-          <div className="flex justify-center">
-              <Link href="/">
-                  <Image src="/logo.png" alt="logo" width={75} height={75} />
+        <ul className="hidden lg:flex items-center gap-6 sm:gap-7">
+          {NAV_LINKS.map((link) => (
+            <li
+              key={link.key}
+              className="text-xs font-sans font-normal tracking-widest uppercase text-gray-50 cursor-pointer transition-all"
+            >
+              <Link href={link.href}>
+                <span className="whitespace-nowrap block pb-.5 border-b border-transparent hover:border-current transition duration-300 ease-in-out">
+                  {link.label}
+                </span>
               </Link>
-          </div>
-          
-          {/* Right-aligned buttons */}
-          <div className="flex items-center justify-end">
-              <Button type="button" icon="/search.png" />
-              <Button type="button" icon="/user.png" />
-              <Button type="button" icon="/cart.png" />           
-          </div>
-      </nav>
-    );
-}
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      {/* Centered logo */}
+      <div className="flex justify-center">
+        <Link href="/">
+          <Image src="/logo.png" alt="logo" width={75} height={75} />
+        </Link>
+      </div>
+
+      {/* Right-aligned buttons */}
+      <div className="flex items-center justify-end">
+        <Button type="button" icon="/search.png" />
+        <Button type="button" icon="/user.png" />
+        <Button type="button" icon="/cart.png" />
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
